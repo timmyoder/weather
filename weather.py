e44822e89b8c1f745e9fdf487f50ed206b1da65a
Tim Yoder <tim.a.yoder@gmail.com>
2020-12-12 12:41:02 -0800

    write to file

--- weather.py
+++ weather.py
@@ -1,15 +1,21 @@
 import serial
 
 port = input("Enter the port name: ")
+baud = int(input('Enter baudrate: '))
 
-ser = serial.Serial(port=port)
-ser.flushInput()
+if baud == "":
+    baud = None
+
+ser = serial.Serial(port=port, baudrate=baud)
+ser.reset_input_buffer()
 
 while True:
     try:
-        ser_bytes = ser.readline()
-        decoded_bytes = ser_bytes.decode("utf-8")
+        bytesToRead = ser.inWaiting()
+        decoded_bytes = ser.read(bytesToRead).decode("utf-8")
         print(decoded_bytes)
+        with open('output.txt', 'a') as out_file:
+            out_file.write(decoded_bytes)
     except KeyboardInterrupt:
         print("Keyboard Interrupt")
         break
